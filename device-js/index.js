01b05df3b69bfdfff4320a0f4214a11c83281140
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2022-12-19 00:24:35 +0100

    Update index.js

--- device-js/index.js
+++ device-js/index.js
@@ -2,8 +2,8 @@ import Realm from "realm";
 import express from 'express';
 
 // Define your data model.
-export const DeviceSchema = {
-    name: 'Device',
+export const RegistrationSchema = {
+    name: 'Registration',
     properties: {
         _id: 'string?',
     },
@@ -20,7 +20,7 @@ await realm_app.logIn(Realm.Credentials.anonymous());
 
 // Create a `SyncConfiguration` object.
 const config = {
-    schema: [DeviceSchema],
+    schema: [RegistrationSchema],
     sync: {
         // Use the previously-authenticated anonymous user.
         user: realm_app.currentUser,
@@ -34,7 +34,7 @@ const config = {
                     // Get objects that match your object model, then filter them
                     // the `owner_id` queryable field
                     //realm.objects("Task").filtered(`owner_id = ${app.currentUser.id}`)
-                    realm.objects("Device")
+                    realm.objects("Registration")
                 );
             },
         },
@@ -45,16 +45,21 @@ const realm = await Realm.open(config);
 const express_app = express()
 const port = 3000
 
-express_app.get('/:id', (req, res) => {
+express_app.get('/exists/:id', (req, res) => {
     res.send(exists(req.params.id));
 })
 
-express_app.listen(port, () => {
-    console.log(`IoT app listening on port ${port}`)
+express_app.get('/register/:id', (req, res) => {
+    res.send(register(req.params.id));
+})
+
+express_app.get('/unregister/:id', (req, res) => {
+    res.send(unregister(req.params.id));
 })
 
 function exists(id) {
-    const result = realm.objectForPrimaryKey("Device", id); // search for a realm object with a primary key that is a string
+    // search for a realm object with a primary key that is a string
+    const result = realm.objectForPrimaryKey("Registration", id);
     console.log("Requested: " + id);
     console.log("Found: " + JSON.stringify(result));
     if (result) {
@@ -63,3 +68,26 @@ function exists(id) {
         return "FALSE";
     }
 }
+
+function register(id) {
+    let result;
+    realm.write(() => {
+        // Create the registration
+        result = realm.create("Registration", { _id: id }, Realm.UpdateMode.All);
+    });
+    // return newly created registration object
+    return result;
+}
+
+function unregister(id) {
+    let result;
+    realm.write(() => {
+        // Delete the registration.
+        result = realm.delete(realm.objectForPrimaryKey("Registration", id));
+    });
+    return result;
+}
+
+express_app.listen(port, () => {
+    console.log(`IoT app listening on port ${port}`)
+})
